82c86594fac095f97131011c6f39bad0f21de4c6
Keti Makhatadze <maxatadzeketi@gmail.com>
2023-07-25 13:02:04 +0400

    Site Bookmarker

--- Part 12/main.js
+++ Part 12/main.js
@@ -3,52 +3,44 @@ document.getElementById('myForm').addEventListener('submit', saveBookmark);
 
 // Save Bookmark
 function saveBookmark(e){
-  // Get form values
-  var siteName =document.getElementById('siteName').value;
-  var siteUrl =document.getElementById('siteUrl').value;
+    // Get form values
+    var siteName =document.getElementById('siteName').value;
+    var siteUrl =document.getElementById('siteUrl').value;
 
-  if(!validateForm(siteName, siteUrl)){
-    return false;
-  }
+    if(!validateForm(siteName, siteUrl)){
+        return false;
+    }
 
-  var bookmark = {
-    name: siteName,
-    url: siteUrl
-  }
+    var bookmark = {
+        name: siteName,
+        url: siteUrl
+    }
 
-  /*
-    // Local Storage Test
-    localStorage.setItem('test', 'Hello World');
-    console.log(localStorage.getItem('test'));
-    localStorage.removeItem('test');
-    console.log(localStorage.getItem('test'));
-  */
+    // Test if bookmarks is null
+    if(localStorage.getItem('bookmarks') === null){
+        // Init array
+        var bookmarks = [];
+        // Add to array
+        bookmarks.push(bookmark);
+        // Set to localStorage
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    } else {
+        // Get bookmarks from localStorage
+        var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+        // Add bookmark to array
+        bookmarks.push(bookmark);
+        // Re-set back to localStorage
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    }
 
-  // Test if bookmarks is null
-  if(localStorage.getItem('bookmarks') === null){
-    // Init array
-    var bookmarks = [];
-    // Add to array
-    bookmarks.push(bookmark);
-    // Set to localStorage
-    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
-  } else {
-    // Get bookmarks from localStorage
-    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
-    // Add bookmark to array
-    bookmarks.push(bookmark);
-    // Re-set back to localStorage
-    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
-  }
+    // Clear form
+    document.getElementById('myForm').reset();
 
-  // Clear form
-  document.getElementById('myForm').reset();
+    // Re-fetch bookmarks
+    fetchBookmarks();
 
-  // Re-fetch bookmarks
-  fetchBookmarks();
-
-  // Prevent form from submitting
-  e.preventDefault();
+    // Prevent form from submitting
+    e.preventDefault();
 }
 
 // Delete bookmark
@@ -58,8 +50,8 @@ function deleteBookmark(url){
   // Loop through the bookmarks
   for(var i =0;i < bookmarks.length;i++){
     if(bookmarks[i].url == url){
-      // Remove from array
-      bookmarks.splice(i, 1);
+        // Remove from array
+        bookmarks.splice(i, 1);
     }
   }
   // Re-set back to localStorage
@@ -110,8 +102,8 @@ function validateForm(siteName, siteUrl){
 }
 
 function addhttp(url) {
-  if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
-      url = "http://" + url;
-  }
+    if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
+        url = "http://" + url;
+    }
   return url;
 }
